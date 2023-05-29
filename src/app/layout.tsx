2d484fb14f5bbe15e7f8377b7731c2449d1bd236
Abdulbasit Saeed <abdulbasitsaeed619@gmail.com>
2023-05-30 00:27:51 +0200

    feat: completed footer design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,8 +20,46 @@ export default function RootLayout({
         <h1>Navbar</h1>
       </div> */}
       <body className={inter.className}>{children}</body>
-      <footer className=" bg-green-300">
-        <h1>footer</h1>
+      <footer className=" bg-gray-100 flex flex-col p-8 gap-8 text-gray-400">
+        <div>
+          <p className="text-black">Address</p>
+          <p>Netherlands - 48E Louis Amstrongweg, 1311RK, Almere</p>
+        </div>
+        <div>
+          <p className="text-black">Say Hello</p>
+          <p>contact@raib.com</p>
+        </div>
+        <div className=" flex flex-col gap-3">
+          <p className="text-black">Social</p>
+          <a href="#" className=" uppercase">
+            facebook
+          </a>
+          <a href="#" className=" uppercase">
+            twitter
+          </a>
+          <a href="#" className=" uppercase">
+            linkedin
+          </a>
+          <a href="#" className=" uppercase">
+            instagram
+          </a>
+        </div>
+        <div>
+          <p>Newsletter</p>
+        </div>
+        <form action="">
+          <input
+            type="email"
+            name=""
+            id=""
+            placeholder="Type your Email"
+            className="w-full bg-transparent border-b border-gray-400 py-2 focus:border-black focus:outline-none"
+          />
+        </form>
+        <br />
+        <hr />
+        <br />
+        <p>&copy; 2023 Raib consultant Etél</p>
       </footer>
     </html>
   );
