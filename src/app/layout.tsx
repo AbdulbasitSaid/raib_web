3b341bb5b1922eaeec61ce3a32f4e644367dad6d
Abdulbasit Saeed <abdulbasitsaeed619@gmail.com>
2023-06-12 02:08:48 +0200

    update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
+import { Alert } from "./components/Alert";
 import Navbar from "./components/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -16,49 +16,40 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Navbar />
-      <body className={inter.className}>{children}</body>
-      <footer className=" bg-gray-100 flex flex-col p-8 gap-8 text-gray-400">
-        <div>
-          <p className="text-black">Address</p>
-          <p>Netherlands - 48E Louis Amstrongweg, 1311RK, Almere</p>
-        </div>
-        <div>
-          <p className="text-black">Say Hello</p>
-          <p>contact@raib.com</p>
-        </div>
-        <div className=" flex flex-col gap-3">
-          <p className="text-black">Social</p>
-          <a href="#" className=" uppercase">
-            facebook
-          </a>
-          <a href="#" className=" uppercase">
-            twitter
-          </a>
-          <a href="#" className=" uppercase">
-            linkedin
-          </a>
-          <a href="#" className=" uppercase">
-            instagram
-          </a>
-        </div>
-        <div>
-          <p>Newsletter</p>
-        </div>
-        <form action="">
-          <input
-            type="email"
-            name=""
-            id=""
-            placeholder="Type your Email"
-            className="w-full bg-transparent border-b border-gray-400 py-2 focus:border-black focus:outline-none"
-          />
-        </form>
-        <br />
-        <hr />
-        <br />
-        <p>&copy; 2023 Raib consultant Etél</p>
-      </footer>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+        <footer className=" bg-gray-100 flex flex-col p-8 gap-8 text-gray-400">
+          <div>
+            <p className="text-black">Address</p>
+            <p>Netherlands - 48E Louis Amstrongweg, 1311RK, Almere</p>
+          </div>
+          <div>
+            <p className="text-black">Say Hello</p>
+            <p>contact@raib.com</p>
+          </div>
+          <div className=" flex flex-col gap-3">
+            <p className="text-black">Social</p>
+            <a href="#" className=" uppercase">
+              facebook
+            </a>
+            <a href="#" className=" uppercase">
+              twitter
+            </a>
+            <a href="#" className=" uppercase">
+              linkedin
+            </a>
+            <a href="#" className=" uppercase">
+              instagram
+            </a>
+          </div>
+
+          <br />
+          <hr />
+          <br />
+          <p>&copy; 2023 Raib consultant Etél</p>
+        </footer>
+      </body>
     </html>
   );
 }
